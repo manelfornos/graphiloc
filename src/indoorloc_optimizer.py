7388dad16cb18681cde3aaa9ee6677bb9c8c6158
Manel Fornos <268213353+manelfornos@users.noreply.github.com>
2026-04-03 09:46:09 +0200

    indoorloc_optimizer.py: set up optuna TPE optimization

--- src/indoorloc_optimizer.py
+++ src/indoorloc_optimizer.py
@@ -21,43 +21,39 @@ class GNNRegressionOptimizer:
 
     def _set_gridparams(self, trial, data, model_class):
         if model_class.__name__ in ['SAGERegressor']:
-
+            n_layers = 2
             if hasattr(data, 'train_mask'):
-                data = data
+                d = data
             else:
-                data = data['train']
-        
-            params = {
-                'input_dim': data.num_features,
-                'output_dim': data.y.shape[1],
-            }   
-            
-            hidden_dims = []
-            dropouts = []
+                d = data['train']
 
-            params['n_layers'] = trial.suggest_categorical('n_layers', [2])
-            for i in range(params['n_layers']):
-                hidden_dims.append(trial.suggest_categorical(f'hidden_dim_layer_{i}', [128, 256]))
-            
-                if i < params['n_layers'] - 1:
-                    dropouts.append(trial.suggest_categorical(f'dropout_layer_{i}', [0.2, 0.4, 0.6]))
-            
-            params['hidden_dim'] = hidden_dims
-            params['dropout'] = dropouts
-            params['learning_rate'] = trial.suggest_categorical('learning_rate', [0.005, 0.01])            
-            params['weight_decay'] = trial.suggest_categorical('weight_decay', [1e-5, 1e-4])            
-            params['optim_factor'] = trial.suggest_categorical('optim_factor', [0.9])
-            params['mlp_layers'] = trial.suggest_categorical('mlp_layers', [2, 4])
-        
-        return params
+            gnn_hidden_dims = []
+            gnn_dropouts = []
+
+            for i in range(n_layers):
+                gnn_hidden_dims.append(trial.suggest_categorical(f'gnn_hidden_dim_layer_{i}', [128, 256]))
+                if i < n_layers - 1:
+                    gnn_dropouts.append(trial.suggest_float(f'gnn_dropout_layer_{i}', 0.1, 0.5))
+
+            params = {
+                'input_dim': d.num_features,
+                'output_dim': d.y.shape[1],
+                'gnn_hidden_dims': gnn_hidden_dims,
+                'gnn_dropouts': gnn_dropouts,
+                'mlp_layers': trial.suggest_categorical('mlp_layers', [2, 4]),
+                'learning_rate': trial.suggest_float('learning_rate', 1e-3, 1e-1, log=True),
+                'weight_decay': trial.suggest_float('weight_decay', 1e-6, 1e-3, log=True),
+                'lr_factor': 0.9,
+            }
+            return params
     
-    def _objective(self, trial, data, model_class):
+    def _objective(self, trial, data, model_class, max_epochs, patience):
         params = self._set_gridparams(trial, data, model_class)
         model = model_class(**params).to(self.device)
         trainer = iltrainer.GNNRegressionTrainer()
 
         mae = trainer.train_validate(
-            data, model, self.max_epochs, self.patience, verbose=0, trial=trial
+            data, model, max_epochs, patience, verbose=0, trial=trial
         )
         
         return mae
@@ -71,6 +67,8 @@ class GNNRegressionOptimizer:
             storage,    
             load_if_exists,
             n_trials,
+            max_epochs,
+            patience,
             callbacks=None
     ):
         
@@ -79,14 +77,16 @@ class GNNRegressionOptimizer:
             direction=direction,
             storage=storage,
             load_if_exists=load_if_exists,
-            pruner=optuna.pruners.MedianPruner(n_startup_trials=5, 
-                                               n_warmup_steps=100,
-                                               n_min_trials=5),
-            sampler=optuna.samplers.TPESampler(seed=SEED)
+            pruner=optuna.pruners.MedianPruner(n_startup_trials=10, 
+                                               n_warmup_steps=200,
+                                               n_min_trials=10),
+            sampler=optuna.samplers.TPESampler(seed=SEED,
+                n_startup_trials=20
+            )
         )
         optuna.logging.set_verbosity(optuna.logging.WARNING)
         study.optimize(
-            lambda trial: self._objective(trial, data, model_class),
+            lambda trial: self._objective(trial, data, model_class, max_epochs, patience),
             n_trials=n_trials,
             n_jobs=1,
             callbacks=callbacks)
